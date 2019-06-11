9ce4f6494b70dcd7b4df1290ebaa1f0f46eef0bf
gab <contact@gabrielcuvillier.pro>
2019-06-11 17:01:23 +0200

    Emscripten: Fixed warning

--- src/Standard/Standard_Mutex.hxx
+++ src/Standard/Standard_Mutex.hxx
@@ -24,7 +24,7 @@
   #include <windows.h>
 #else
   #include <pthread.h>
-  #if defined(EMSCRIPTEN)
+  #if defined(__EMSCRIPTEN__)
     #include <errno.h>  // including <errno.h> instead of incorrect <sys/errno.h>
   #else
     #include <sys/errno.h>
